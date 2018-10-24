0dd8b0376a4d054e9ab65edf604f5c004aae9dd1
Can Serif Mekik <can.mekik@gmail.com>
2018-10-24 09:48:01 -0400

    reworked transformation.py

--- pyRavenMatrices/transformation.py
+++ pyRavenMatrices/transformation.py
@@ -20,174 +20,123 @@ modifiers from figures in addition to having other effects such as addition of
 elements to or removal of elements from figures.
 '''
 
+
 import copy
 from typing import Callable, Any, List, cast
-from pyRavenMatrices.element import (
-    ElementNode,
-    Element,
-    ElementModifier, 
-    BasicElement, 
-    EmptyElement, 
-    ModifiedElement, 
-    CompositeElement,
-    get_subtrees
-)
+
+
+class Target(object):
+    
+    def __init__(
+        self, 
+        attribute: str = None, 
+        index: int = None, 
+        parent: 'Target' = None, 
+        type: type = None
+    ) -> None:
+        
+        self.attribute = attribute
+        self.index = index
+        self.parent = parent
+        self.type = type
+        
+    def __repr__(self):
+        
+        details = self._repr()
+        if self.type is not None:
+            type_annotation = ', type={}'.format(self.type.__name__)
+        else:
+            type_annotation = ''
+        return 'Target({}{})'.format(details, type_annotation)
+    
+    def __eq__(self, other):
+        
+        return (
+            self.attribute == other.attribute,
+            self.index == other.index,
+            self.parent == other.parent,
+            self.type == other.type
+        )
+
+    def __call__(self, element):
+        
+        target = element
+        if self.parent is not None:
+            target = parent(element)
+        if self.attribute is not None:
+            target = getattr(target, self.attribute)
+        if self.index is not None:
+            target = target[self.index]
+        return target
+    
+    def _repr(self):
+        
+        if self.parent is not None:
+            parent = self.parent._repr()
+        else:
+            parent = 'root'
+            
+        if self.attribute is not None:
+            attribute = '.' + self.attribute
+        else:
+            attribute = ''
+            
+        if self.index is not None:
+            index = str(self.index).join(['[',']'])
+        else:
+            index = ''
+            
+        return ''.join([parent, attribute, index])
 
 
 class Transformation(object):
-    '''Represents a Raven-style figure transformation.'''
     
-    def __init__(self, *args):
-        '''
-        Initialize transformation instance
+    def __init__(self, *triples):
         
-        Every arg is assumed to have the form:
-
-        ``(condition, action)``
-        
-        ``condition`` is an element and ``action`` is a tuple
-        to of the form ``(op, loc, *args, **kwarg)``, where ``op`` is the
-        desired operation ``loc`` points to the location in an element at 
-        which the operation should be executed and ``*args``` are a list of 
-        arguments to be given that operation.        
-        '''
-        
-        self.pairs = args
+        self.triples = list(triples)
         
     def __call__(self, element):
-        '''Transform ``element``.'''
         
         output = copy.deepcopy(element)
-        for condition, action in self.pairs:
-            if condition in get_subtrees(element):
-                output = transform(output, *action)
+        for target, pattern, value in self.triples:
+            if target(element) == pattern:
+                if issubclass(target.type, elt.BasicElement):
+                    target(output).routine = value['routine']
+                else:
+                    target(output).decorator = value['decorator']
+                target(output).params = value['params']
         return output
 
 
-def transform(
-    element : ElementNode, 
-    op : Callable[..., ElementNode], 
-    loc : ElementNode, 
-    *args : Any, 
-    **kwargs : Any
-) -> ElementNode:
-    '''
-    Apply ``op`` to every occurrence of ``loc`` in ``element``.
-
-    Note: Calls to ``op`` have the following form:
-    ``op(loc, *args, **kwargs)``
-
-    :param ``element``: Target element for transformation.
-    :param ``op``: Operation for altering ``element``.
-    :param ``loc``: Subelement of ``element`` to be modified.
-    :param ``*args``: Args to be passed on to ``op``.
-    :param ``**kwargs``: Kwargs to be passed on to ``op``.
-    '''
+def get_targets(element_structure, parent=None):
     
-    # Implemented using recursion.
-
-    if element == loc: # apply op; return result.
-    
-        ret = op(element, *args, **kwargs)
-    
-    else: # propagate to subelements (if any) and return result
-        
-        if isinstance(element, (BasicElement, ElementModifier)):
-            ret = element
-
-        elif isinstance(element, ModifiedElement):
-            new_subelt = transform(element.element, op, loc, *args, **kwargs)
-            if new_subelt and isinstance(new_subelt, Element):
-                new_mods = [
-                    tfd for tfd in (
-                        transform(mod, op, loc, *args, **kwargs) for mod in 
-                        element.modifiers
-                    ) if isinstance(tfd, ElementModifier)
-                ]
-                if new_mods:
-                    ret = ModifiedElement(new_subelt, *new_mods)
-                else:
-                    ret = new_subelt
-            else:
-                ret = EmptyElement()
-            
-        elif isinstance(element, CompositeElement):
-            new_elts = [
-                elt for elt in (
-                    transform(elt, op, loc, *args, **kwargs) for elt in 
-                    element.elements
-                ) if (
-                    isinstance(elt, Element) and 
-                    not isinstance(elt, EmptyElement)
-                )
-            ]
-            if len(new_elts) > 1:
-                ret = CompositeElement(*new_elts)
-            elif len(new_elts) == 1:
-                ret = new_elts.pop()
-            else:
-                ret = EmptyElement()
-
-    return ret
-            
-def add(element : ElementNode, addition : ElementNode):
-    '''Add ``addition`` to ``element`` and return result.'''
-
-    # Should add addition to element and return result
-    
-    # **Deepcopy** for safe in-place ops
-    output = copy.deepcopy(element)
-    
-    if (
-        isinstance(output, (BasicElement, ModifiedElement)) and 
-        isinstance(addition, Element)
-    ):
-        output = CompositeElement(output, addition)
-    elif (
-        isinstance(output, (BasicElement, CompositeElement)) and 
-        isinstance(addition, ElementModifier)
-    ):
-        output = ModifiedElement(output, addition)
-    elif (
-        isinstance(output, CompositeElement) and 
-        isinstance(addition, Element)
-    ):
-        output.elements.append(addition)
-    elif (
-        isinstance(output, ModifiedElement) and 
-        isinstance(addition, ElementModifier)
-    ):
-        output.modifiers.append(addition)
-    
-    return output
-
-
-def remove(element : ElementNode):
-    '''Remove ``element``.'''
-    
-    return EmptyElement()
-
-
-def replace(element : ElementNode, replacement : ElementNode):
-    '''Replace ``element`` with ``replacement``.'''
-    
-    if (
-        isinstance(element, Element) and 
-        not isinstance(replacement, Element)
-    ): 
-        raise TypeError(
-            'Expected Element, got {}'.format(
-                type(replacement).__name__
-            )
-        )        
-    elif (
-        isinstance(element, ElementModifier) and
-        not isinstance(replacement, ElementModifier)
-    ):
-        raise TypeError(
-            'Expected ElementModifier, got {}'.format(
-                type(replacement).__name__
+    if isinstance(element_structure, (elt.BasicElement, elt.ElementModifier)):
+        if parent:
+            return [parent]
+        else:
+            return [Target(type=type(element_structure))]
+    elif isinstance(element_structure, elt.ModifiedElement):
+        sub_element = element_structure.element
+        ret = get_targets(
+            sub_element, 
+            parent = Target(
+                'element', 
+                parent = parent, 
+                type = type(sub_element)
             )
         )
-    
-    return replacement
+        for i, modifier in enumerate(element_structure.modifiers):
+            ret += get_targets(
+                modifier, 
+                Target('modifiers', index=i, parent=parent, type=type(modifier))
+            )
+    elif isinstance(element_structure, elt.CompositeElement):
+        ret = []
+        for i, sub_element in enumerate(element_structure.elements):
+            ret += get_targets(
+                sub_element, 
+                Target(
+                    'elements', index=i, parent=parent, type=type(sub_element)
+                )
+            )
+    return ret
